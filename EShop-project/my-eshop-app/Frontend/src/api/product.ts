077fabe041aaf240180e92dc6e8d31ecedfc219a
ofek4643 <ofekddia1@gmail.com>
2025-09-04 00:38:25 +0300

    first check render

--- EShop-project/my-eshop-app/Frontend/src/api/product.ts
+++ EShop-project/my-eshop-app/Frontend/src/api/product.ts
@@ -1,24 +1,20 @@
 import axios from "axios";
 import { Product } from "../types/Product";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/";
+
 // משיכת המוצרים מהשרת
 export const fetchProducts = async (): Promise<Product[]> => {
-  const res = await axios.get<Product[]>(
-    "https://eshop-project-react.onrender.com/api/product",
-    {
-      withCredentials: true,
-    }
-  );
+  const res = await axios.get<Product[]>(`${BASE_URL}product`, {
+    withCredentials: true,
+  });
   return res.data;
 };
 
 // משיכת מוצר על פי ID
 export const fetchProductById = async (id: string): Promise<Product> => {
-  const res = await axios.get<Product>(
-    `http://localhost:5000/api/product/${id}`,
-    {
-      withCredentials: true,
-    }
-  );
+  const res = await axios.get<Product>(`${BASE_URL}product/${id}`, {
+    withCredentials: true,
+  });
   return res.data;
 };
